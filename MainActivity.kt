8e767df1f28a1a57f1e97c9cc3c3ce54113147fe
FinalProject777 <152974269+FinalProject777@users.noreply.github.com>
2023-12-14 16:20:27 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,57 +1,43 @@
-package com.example.loginpage
+package com.example.myapplication1
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private var backPressedOnce = false
+
+    private val TAG = "MainActivity"
+    private val db = Firebase.firestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
-        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
-        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
-        val clickableText =
-            findViewById<TextView>(R.id.clickableText) // Moved here for accessibility
-        val x = "sashi"
-        val y = "sashi123"
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
 
-        buttonLogin.setOnClickListener {
-            val username = editTextUsername.text.toString()
-            val password = editTextPassword.text.toString()
-
-            // Replace the condition with your actual authentication logic
-            if (username == x && password == y) {
-                // Successful login
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                // Failed login
-                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+        // Add a new document with a generated ID
+        db.collection("demo")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
             }
-        }
-
-        // Set click listener for clickableText
-        clickableText.setOnClickListener {
-
-            val intent = Intent(this, adminmain::class.java)
-            startActivity(intent)
-        }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        // Close the current activity when the back button is pressed
-        finishAffinity()
-    }
-
-
 }
+
+
+
+
+
+
+
+
